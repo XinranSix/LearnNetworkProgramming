7d908b3522db85f9d8e727d1e2f75a961ad5bb7f
Yao J <jieyao.lxr@foxmail.com>
2023-07-26 17:47:23 +0800

    uopdate

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,12 @@
 add_rules("mode.debug", "mode.release")
 
+if is_plat("windows") then
+    add_cxflags("/utf-8", "/std:c++20")
+    add_links("ws2_32")
+else
+    add_cxflags("-fexec-charset=UTF-8", "-fextended-identifiers", "-finput-charset=UTF-8")
+end
+
 for _, filepath in ipairs(os.files("./**.c")) do
 target(path.basename(filepath))
     add_files(filepath)
